4c55bc3ebd31fc11141d1ae519ae2ce47b4d2707
VladimirSokurenko <dp240191svv1@gmail.com>
2019-08-18 07:19:17 +0300

    web_driver_set_up

---
+++ src/test/java/webDriverSetUp/WebDriverSetUp.java
@@ -0,0 +1,28 @@
+package webDriverSetUp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverSetUp {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("chromedriver", "/home/alexander/IdeaProjects/salesforce_selenium_test");
+        driver = new ChromeDriver();
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+
+
+
+
+
+}
